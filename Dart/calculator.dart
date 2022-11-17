234b87512e454175fc21e59ee48ba9e85127f048
Edward Shinyangane Ndlala <Edwardthina@gmail.com>
2022-11-17 14:56:33 +0200

    week 1 mini project

---
+++ Dart/calculator.dart
@@ -0,0 +1,33 @@
+import 'dart:ffi';
+import 'dart:io';
+
+class calculator {
+  
+  void start(){
+    String str = "";
+    Double _number = 0;
+    
+   while(str != "exit" || str != "EXIT"){
+   print("enter numbers and sign");
+   stdout.writeln('Enter your name: ');   //Asking the name as output
+
+  
+
+   }
+  }
+  
+  int sum = 0;
+  int a = 0;
+  int b = 0;
+
+  // properties and methods
+  int add(int a, int b){
+    sum = a+b;
+    return sum;
+  }
+ int subtract(int a, int b){
+   sum = a - b; 
+   return sum;
+  }
+  
+}
