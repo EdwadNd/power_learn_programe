32c25a86536b37cd6656a911419b25c72d16963e
Edward Shinyangane Ndlala <Edwardthina@gmail.com>
2022-11-19 14:18:19 +0200

    python self test week 3

---
+++ PYTHON/dictionarySelftest.py
@@ -0,0 +1,8 @@
+personalDetails = {
+    "FisrtName" : "James","LastName" : "Ouko","Age" : 22,"PhoneNumber" : "07123456789", "Email" : "student@gmail.com"}
+personalDetails["Age"] = 55 #Update Age to 55
+print(personalDetails["Age"])
+print(personalDetails["LastName"]) #Print the LastName  
+print(personalDetails["FisrtName"]) #Print the firsttName  
+
+print(len(personalDetails)) #Print length of the dictionary
--- web development/javascript/script.js
+++
@@ -1,7 +0,0 @@
-let str = 'string'
-let num = 55
-let state = true
-var name = 'Edward'
-
-const Edward = 'myself'
-console.log(str , num, state, naame)
